059622ce661d22bf095dd6668a3659ef15204bdb
nukeop <nukeop@gumblert.tech>
2019-02-24 20:16:06 +0100

    notes parts reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
+import Notes from './notes';
 import Notifications from './notifications';
 import User from './user';
 
 export default combineReducers({
+  notes: Notes,
   notifications: Notifications,
   user: User
 });
--- src/reducers/notes.js
+++ src/reducers/notes.js
@@ -1,16 +1,23 @@
 import {
-
+  GET_NOTES_PARTS_START,
+  GET_NOTES_PARTS_SUCCESS
 } from '../actions/notes';
 
 const initialState = {
   notes: {},
-  structures: [],
-  conjunctions: [],
-  phrases: []
+  parts: {}
 };
 
 export default function NotesReducer(state=initialState, action) {
   switch(action.type) {
+  case GET_NOTES_PARTS_START:
+    return Object.assign({}, state, {
+      parts: { loading: true }
+    });
+  case GET_NOTES_PARTS_SUCCESS:
+    return Object.assign({}, state, {
+      parts: action.payload.parts
+    });
   default:
     return state;
   }
