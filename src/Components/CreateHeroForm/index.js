d496ac85380b231d20df1029e2dee600efdb15c1
nukeop <nukeop@gumblert.tech>
2019-04-24 01:20:53 +0200

    Create hero form in one component

--- src/Components/CreateHeroForm/index.js
+++ src/Components/CreateHeroForm/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
 import {
+  Button,
   Container,
   Divider,
   Dropdown,
@@ -58,124 +59,131 @@ const CreateHeroForm = props => {
       <Grid.Row centered>
      
         <Grid.Column>
-          <Container>
+          <Container text>
             <Segment inverted loading={ _.get(definitions, 'loading') }>
               <Header inverted>
               Create a hero
               </Header>
               <Divider />
-              <Grid.Row divided columns={2}>
+              <Form inverted as={ Grid.Row } divided columns={ 2 }>
                 <Grid.Column>
-                  <Form inverted>
-                    <Form.Field>
-                      <label>Class</label>
-                      <Dropdown
-                        selection
-                        fluid
-                        options={
-                          _.map(definitions.heroclasses, classToOption)
-                        }
-                        value={ _.get(selectedClass, 'name') }
-                        onChange={ (e, item) => setSelectedClass(
-                          _.find( definitions.heroclasses, { name: item.value })
-                        ) }
-                      />
-                    </Form.Field>
-                    <Form.Field>
-                      <label>Name</label>
-                      <Input
-                        inverted
-                        fluid
-                        placeholder='Name...'
-                      />
-                    </Form.Field>
-                  </Form>
+                  <Form.Field>
+                    <label>Class</label>
+                    <Dropdown
+                      selection
+                      fluid
+                      options={
+                        _.map(definitions.heroclasses, classToOption)
+                      }
+                      value={ _.get(selectedClass, 'name') }
+                      onChange={ (e, item) => setSelectedClass(
+                        _.find( definitions.heroclasses, { name: item.value })
+                      ) }
+                    />
+                  </Form.Field>
                 </Grid.Column>
                 <Grid.Column>
-                  <Header inverted as='h3'>
+                  <Form.Field>
+                    <label>Name</label>
+                    <Input
+                      inverted
+                      fluid
+                      placeholder='Name...'
+                    />
+                  </Form.Field>
+                </Grid.Column>
+              </Form>
+
+              <Divider />
+              
+              <Grid.Column>
+                <Header inverted as='h3'>
                     Starting attributes
-                  </Header>
-                  <HeroStatsLine
-                    name='HP'
-                    value={ _.get(selectedClass, 'baseHp') }
-                  />
-                  <HeroStatsLine
-                    name='Attack'
-                    value={ _.get(selectedClass, 'baseAttack') }
-                  />
-                  <HeroStatsLine
-                    name='Defense'
-                    value={ _.get(selectedClass, 'baseDefense') }
-                  />
-                  <Divider />
-                  <Header inverted as='h3'>
+                </Header>
+                <HeroStatsLine
+                  name='HP'
+                  value={ _.get(selectedClass, 'baseHp') }
+                />
+                <HeroStatsLine
+                  name='Attack'
+                  value={ _.get(selectedClass, 'baseAttack') }
+                />
+                <HeroStatsLine
+                  name='Defense'
+                  value={ _.get(selectedClass, 'baseDefense') }
+                />
+                <Divider />
+                <Header inverted as='h3'>
                     Slots
-                  </Header>
-                  <Label.Group color='blue'>
-                    {
-                      _.map(
-                        _.get(selectedClass, 'slots'), slot => {
-                          return (
-                            <Label>
-                              {
-                                _.get(
-                                  _.find(
-                                    _.get(definitions, 'equipmentslots'),
-                                    { id: slot }
-                                  ), 'name')
-                              }
-                            </Label>
-                          );
-                        })
-                    }
-                  </Label.Group>
-                  <Divider />
-                  <Header inverted as='h3'>
-                    Abilities
-                  </Header>
+                </Header>
+                <Label.Group color='blue'>
                   {
                     _.map(
-                      _.get(selectedClass, 'abilities'), ability => {
-                        const abilityDef = _.find(
-                          _.get(definitions, 'abilities'),
-                          { id: ability }
-                        );
+                      _.get(selectedClass, 'slots'), slot => {
                         return (
-                          <Header inverted as='h4'>
-                            { _.get(abilityDef, 'prettyName') }
-                            <Header.Subheader>
-                              { _.get(abilityDef, 'description') }
-                            </Header.Subheader>
-                          </Header>
-                        );
-                      }
-                    )
-                  }
-                  <Divider />
-    
-                  <Header inverted as='h3'>
-                    Moves
-                  </Header>
-                  {
-                    _.map(
-                      _.get(selectedClass, 'moves'), move => {
-                        const moveDef = _.find(
-                          _.get(definitions, 'moves'),
-                          { id: move }
-                        );
-                        return (
-                          <Header inverted as='h4' className={ styles.hero_class_move }>
-                            { _.get(moveDef, 'prettyName') }
-                            <Header.Subheader>
-                              { _.get(moveDef, 'description') }
-                            </Header.Subheader>
-                          </Header>
-                              
+                          <Label>
+                            {
+                              _.get(
+                                _.find(
+                                  _.get(definitions, 'equipmentslots'),
+                                  { id: slot }
+                                ), 'name')
+                            }
+                          </Label>
                         );
                       })
                   }
-                </Grid.Column>
-              </Grid.Row>
+                </Label.Group>
+                <Divider />
+                <Header inverted as='h3'>
+                    Abilities
+                </Header>
+                {
+                  _.map(
+                    _.get(selectedClass, 'abilities'), ability => {
+                      const abilityDef = _.find(
+                        _.get(definitions, 'abilities'),
+                        { id: ability }
+                      );
+                      return (
+                        <Header inverted as='h4'>
+                          { _.get(abilityDef, 'prettyName') }
+                          <Header.Subheader>
+                            { _.get(abilityDef, 'description') }
+                          </Header.Subheader>
+                        </Header>
+                      );
+                    }
+                  )
+                }
+                <Divider />
+    
+                <Header inverted as='h3'>
+                    Moves
+                </Header>
+                {
+                  _.map(
+                    _.get(selectedClass, 'moves'), move => {
+                      const moveDef = _.find(
+                        _.get(definitions, 'moves'),
+                        { id: move }
+                      );
+                      return (
+                        <Header inverted as='h4' className={ styles.hero_class_move }>
+                          { _.get(moveDef, 'prettyName') }
+                          <Header.Subheader>
+                            { _.get(moveDef, 'description') }
+                          </Header.Subheader>
+                        </Header>
+                              
+                      );
+                    })
+                }
+              </Grid.Column>
+              <Divider />
+              <Button  primary>
+                Create
+              </Button>
             </Segment>
           </Container>
         </Grid.Column>
